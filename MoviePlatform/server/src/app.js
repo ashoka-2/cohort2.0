81124b4ba67c70dc38ec3c8ced500f9eda0337d9
Ashok Kumar <choudharyashok1230@gmail.com>
2026-03-09 13:31:04 +0530

    Some more few fixes

--- MoviePlatform/server/src/app.js
+++ MoviePlatform/server/src/app.js
@@ -7,13 +7,16 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://moodplay-ai.vercel.app/",
-      "https://moodplay-ai.vercel.app",
-      "http://localhost:5173",
-    ],
+      "https://moodplay-ai.vercel.app", 
+      "https://moodplay-ai.vercel.app/", 
+      "http://localhost:5173"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+
+app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
